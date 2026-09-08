ed74e6c756f2f4bfafc1a6a4cbb5e04995b2a39a
xiaoyuesu96-beep <xiaoyuesu96@gmail.com>
2026-09-08 10:38:08 +0200

    add sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+/* 苏晓萍习惯打卡 - Service Worker：让网页安装成 App 后可以离线使用 */
+var CACHE = 'sxp-checkin-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request).then(function (res) {
+        // 同源成功响应写入缓存
+        if (res && res.ok && e.request.url.indexOf(self.location.origin) === 0) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        // 离线时兜底返回首页
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
